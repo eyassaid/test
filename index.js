38b179372c048eac68f347651b063de4f73570d0
eyas <eyas@windows.local>
2022-12-21 01:54:54 +0400

    user can enter their name

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("حييهم")
+var name = prompt("ادخل اسمك")
+
+
+if (name != "null")
+document.body.querySelector("h1").textContent = "اهلا و سهلا " + name;
+else
+document.body.querySelector("h1").textContent = "اهلا و سهلا ";
